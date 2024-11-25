1145b1917f51c88e3766170174f2d2612fcd47a5
Rostislav Alekseev <mr.rostik.01@gmail.com>
2024-11-25 20:06:01 +0200

    disable blur on video player preview

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("Unblur VK videos");
+const targetNode = document.body;
+const config = { attributes: true, childList: true, subtree: true };
+
+const callback = function (mutationsList, observer) {
+  mutationsList.forEach((mutation) => {
+    if (mutation.type === "attributes" && mutation.target.classList.contains("videoplayer--blur")) {
+      mutation.target.classList.remove("videoplayer--blur");
+    }
+  });
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
+
+window.addEventListener("beforeunload", () => {
+  observer.disconnect();
+});
